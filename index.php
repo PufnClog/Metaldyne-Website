811f32a3409025d833b0a9db614fc8b53303e8cb
Zoe Wood <zwood1992@gmail.com>
2012-09-29 14:51:38 +0100

    Lets get started

---
+++ index.php
@@ -0,0 +1,110 @@
+
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Metaldyne.co.uk</title>
+            <!-- Bootstrap -->
+            <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        </head>
+        <body>
+
+        <!-- JS & JQuery-->
+
+            <script src="http://code.jquery.com/jquery-latest.js"></script>
+            <script src="bootstrap/js/bootstrap.min.js"></script>
+
+                <!--The HTML (starts here apparantly)-->
+
+            <div class="container">
+
+        <!--Header Content-->
+
+                <div class="band"></div>
+                <div class="header">
+                    <div class="logo">
+                        <img class="metallogo" src="metaldynelogo.jpg" alt="logo" />
+                    </div>
+                    <div class="search"></div>
+                </div>
+
+        <!--Navigation-->
+
+                <div class="navbar navbar-static-top">
+                    <div class="navbar-inner">
+                        <a class="brand" href="#"><i class="icon-home icon-white"></i></a>
+                            <ul class="nav">
+                                <!--<li class="active"><a href="#"><b>Home</b></a></li>-->
+                                <li><a href="#">Link1</a></li>
+                                <li><a href="#">Link2</a></li>
+                                <li><a href="#">Link3</a></li>
+                                <li><a href="#">Contact us</a></li>
+                            </ul>
+                    </div>
+                </div>
+
+        <!--Image Slides JQuery-->
+
+                <div class="slide">
+                    <div id="myCarousel" class="carousel slide">
+                        <!-- Slide Content -->
+                        <div class="carousel-inner">
+                          <div class="active item"><img class="metallogo1" src="metaldynelogo.jpg" alt="img1" /></div>
+                          <div class="item"><img class="metallogo1" src="metaldynelogo.jpg" alt="img2" /></div>
+                          <div class="item"><img class="metallogo1" src="metaldynelogo.jpg" alt="img3" /></div>
+                        </div>
+                        <!-- Slide Arrows -->
+                        <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                        <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+                    </div>
+                </div>
+
+        <!--Website content-->
+
+                <div class="content">
+                    <div class="titlea">
+                        <h3>Welcome</h3>
+                    </div>
+                    <div class="info">
+                        <p>We are a well-established global leader in the supply of viscous dampers with a renowned reputation for our technical
+                        capability and quality product having a credible portfolio of customers. Special Products, Halifax, specialises in the
+                        supply of low volume requirements, designing, manufacture and repair of viscous dampers for niche engine markets without
+                        any restrictions of a large organisation. We specialise in repair & servicing viscous crankshaft dampers and camshaft
+                        dampers worldwide for end users and engine manufactures. Metaldyne / Holset viscous dampers are our specialty but dampers
+                        manufactured by S.T.E. and Hasse & Wrede can also be repaired.
+                        Basic overhaul can be generally completed within 24 / 48 hours of arrival at our factory. </p>
+                    </div>
+                </div>
+                <div class="line"></div>
+                <div class="plist">
+                    <div class="titleb">
+                        <h3>Product List</h3>
+                    </div>
+                    <div class="subinfo">
+                        <p>Designers and manufactures of Torsional Viscous vibration engine dampers
+                            </br>
+                            </br>
+                            Silicone fluid analysis testing & results report within 24 hours
+                            </br>
+                            </br>
+                            Damper Repair UK / Worldwide
+                            </br>
+                            </br>
+                            Off site damper repair / re-conditioning for dampers over 1.5 metre
+                            </br>
+                            </br>
+                            Exchange dampers available
+                            </br>
+                            </br>
+                            New dampers available</p>
+                    </div>
+                </div>
+
+        <!--Footer Nav -->
+
+                <div class="footer">
+                    <p>Created By Zoe Wood 2012</p>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </body>
+    </html>
